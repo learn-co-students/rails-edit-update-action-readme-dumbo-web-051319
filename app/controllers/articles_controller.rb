db8cffb0fc6a52c715fb863b318fa8e9977893ad
Joe Jamsky <cba1067950@yahoo.com>
2019-06-11 14:15:04 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,17 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:description])
+    redirect_to article_path(@article)
+  end
+
+  def strong_params
+    params.require(@article).permit(:title, :description)
+  end
 end
